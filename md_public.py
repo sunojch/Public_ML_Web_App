fbdbc2ee955dcd757ec10c9cdfc5f538f36e5ae1
sunojch <123616343+sunojch@users.noreply.github.com>
2023-01-26 13:20:11 +0530

    Update md_public.py

--- md_public.py
+++ md_public.py
@@ -13,6 +13,8 @@ from streamlit_option_menu import option_menu
 # loading the saved models
 
 lung_cancer_model = pickle.load(open('trained_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
